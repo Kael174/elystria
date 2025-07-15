1c5af7791ae87068b5eab8990bcc8ca69b145de4
Kael174 <kaelwindshard174@gmail.com>
2025-07-16 01:39:12 +0300

    script.js

--- blog/script.js
+++ blog/script.js
@@ -1,1 +1,9 @@
-alerth("geliştirme aşamasında")
+<script>
+  function gelistirmeAsamasinda() {
+    alert('Bu bölüm henüz yapım aşamasında!');
+    window.location.href = 'index.html';
+  }
+</script>
+
+<button onclick="gelistirmeAsamasinda()">🔮 Mistik Blog</button>
+<button onclick="gelistirmeAsamasinda()">⚙️ Teknoloji Köşesi</button>
