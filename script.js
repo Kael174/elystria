73dad6d5f900be4d0b180690370f62a0389ac71a
Kael174 <kaelwindshard174@gmail.com>
2025-07-16 02:03:51 +0300

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const modeToggle = document.getElementById('modeToggle');
 
-  function gelistirmeAsamasinda() {
-    alert('Bu bölüm henüz yapım aşamasında!');
-  }
+function gelistirmeasamasinda() {
+  alert("bu bölüm geliştirme aşamasındadır")
+}
+function gelistirmeAsamasinda() {
+  alert("bu bölüm geliştirme aşamasındadır")
+}
 
-modeToggle.addEventListener('click', () => {
+  modeToggle.addEventListener('click', () => {
   document.body.classList.toggle('light-mode');
 
   if(document.body.classList.contains('light-mode')) {
