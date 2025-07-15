0ad6de6b20397521f80e630495a5a4efcc44f544
Kael174 <kaelwindshard174@gmail.com>
2025-07-16 01:43:48 +0300

    script.js

--- teknoloji/script.js
+++ teknoloji/script.js
@@ -3,7 +3,4 @@
     alert('Bu bölüm henüz yapım aşamasında!');
     window.location.href = 'index.html';
   }
-</script>
-
-<button onclick="gelistirmeAsamasinda()">🔮 Mistik Blog</button>
-<button onclick="gelistirmeAsamasinda()">⚙️ Teknoloji Köşesi</button>
+  </script>
